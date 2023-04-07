e27eb2d5db30fbcaab3ac3552baf30af093680d9
Vijay Jayaram <ranic@users.noreply.github.com>
2023-04-06 23:55:41 -0700

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/ranic/documentation-starter-kit',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/ranic/documentation-starter-kit',
   footer: {
     text: 'Nextra Docs Template',
   },
