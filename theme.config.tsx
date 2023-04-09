0609ee9318ef5b38a82e2bc5c91c031fb907c6c4
Vijay Jayaram <vijay.jayaram@mixpanel.com>
2023-04-09 12:48:28 -0700

    Revert "stuff"
    This reverts commit 7ea5362a0f9855e9c3ddb37092eb1d91487c9f44.

--- theme.config.tsx
+++ theme.config.tsx
@@ -23,7 +23,14 @@ const config: DocsThemeConfig = {
     titleComponent({ title, type, route }) {
       if (type !== 'separator' && route.match(/\//g) && route.match(/\//g).length == 1) {
       	return (
-		    <li style='nx-mt-5 nx-mb-2 nx-px-2 nx-py-1.5 nx-text-sm nx-font-semibold nx-text-gray-900 first:nx-mt-0 dark:nx-text-gray-100'>
+		    <li
+		      className={cn(
+		        '[word-break:break-word]',
+		        title
+		          ? 'nx-mt-5 nx-mb-2 nx-px-2 nx-py-1.5 nx-text-sm nx-font-semibold nx-text-gray-900 first:nx-mt-0 dark:nx-text-gray-100'
+		          : 'nx-my-4'
+		      )}
+		    >
             {title}
 		    </li>
 		  )
